880074397a558714ce931297e1d4ce1ee470330f
negar.a <negaarabolhasani@gmail.com>
2026-05-05 16:10:13 +0330

    fix: clean up input and output

--- weather.php
+++ weather.php
@@ -4,9 +4,19 @@ use Negar\WeatherApp\WeatherService;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+if ($argc !=2) {
+    echo "Usage: php weather.php <city> <temp>\n";
+    exit(1);
+}
 $WeatherService = new WeatherService();
 
-$city = "tehran";
+$city = $argv["1"];
+echo "Getting information for the weather in $city\n";
 $weather = $WeatherService->getWeather($city);
 
-var_dump($weather);
+
+echo "\n";
+echo "City: " . $weather["city"] . "\n";
+echo "Description: " . $weather["description"] . "\n";
+echo "Temperature: " . $weather["temp"] . "\n";
+echo "Humidity: " . $weather["humidity"] . "\n";
